d843daf6b303e1645abccf105151339c5719317f
wesenbergg <b.b.j.1998@gmail.com>
2020-12-31 19:49:56 +0200

    Client error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,10 +21,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(
       res => {
         this.router.navigateByUrl('/members');
-      },
-      err => {
-        console.log(err);
-        this.toastr.error(err.error);
       });
   }
 
