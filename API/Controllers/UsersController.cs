f979403ddd15109f4c190bd8ce1e6385f49a7993
wesenbergg <b.b.j.1998@gmail.com>
2021-01-04 16:06:55 +0200

    api update req

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -35,5 +36,20 @@ namespace API.Controllers
       return await _userRepository.GetMemberAsync(username);
     }
 
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+      //Get username according to signature in headers
+      var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var user = await _userRepository.GetUserByUsernameAsync(username);
+
+      _mapper.Map(memberUpdateDto, user);
+
+      _userRepository.Update(user);
+
+      if(await _userRepository.SaveAllAsync()) return NoContent();
+
+      return BadRequest("Failed to update user.");
+    }
   }
 }
